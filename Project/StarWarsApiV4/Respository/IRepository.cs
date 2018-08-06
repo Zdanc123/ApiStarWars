39db2d775f134f2c7f29bdc74b88676fe64f62b3
Mysoon <zdancewicz5@interia.pl>
2018-08-06 14:14:27 +0200

    addRepositories

---
+++ Project/StarWarsApiV4/Respository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace StarWarsApiV4.Respository
+{
+    interface IRepository < T > where T : class
+    {
+        IEnumerable<T> getAll();
+        T get(int id);
+        T insert(T obj);
+        T put(T obj);
+        void delete(int id);
+
+    }
+}
---
+++ Project/StarWarsApiV4/Respository/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarWarsApiV4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace StarWarsApiV4.Respository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SWContext context;
+        private DbSet<T> entities;
+
+
+        public Repository(SWContext context)
+        {
+            this.context = context;
+            entities = context.Set<T>();
+        }
+
+        public IEnumerable<T> getAll()
+        {
+
+            return entities;
+        }
+
+        public T get(int id)
+        {
+            return entities.Find(id);
+        }
+
+
+        public T insert(T obj)
+        {
+            entities.Add(obj);
+            context.SaveChanges();
+            return obj;
+
+        }
+
+        public T put(T obj)
+        {
+            entities.Attach(obj);
+            context.Entry(obj).State = EntityState.Modified;
+            context.SaveChanges();
+            return obj;
+        }
+
+      
+
+        public void delete(int id)
+        {
+            entities.Remove(entities.Find(id));
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
